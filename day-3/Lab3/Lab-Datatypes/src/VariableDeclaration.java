bc97f25b7f2d20378a35579ba8ecb5a3a1a07bfb
Mahmoud Alminawi <mahmoud@alminawi.org>
2021-06-23 22:07:39 -0400

    added lab 3

---
+++ day-3/Lab3/Lab-Datatypes/src/VariableDeclaration.java
@@ -0,0 +1,22 @@
+public class VariableDeclaration {
+
+    public static void main(String[] args){
+        boolean on = false;
+        System.out.println(on);
+
+        short s = 16;
+        System.out.println(s);
+
+        int i = -32;
+        System.out.println(i);
+ 
+        float f = 3839.34839f;
+        System.out.println(f);
+
+        long l = 4294967296L;
+	    System.out.println(l);
+	
+	    char c = 'a';
+	    System.out.println(c);
+    }
+}
