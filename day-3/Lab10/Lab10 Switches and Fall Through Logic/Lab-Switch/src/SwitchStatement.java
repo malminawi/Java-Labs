8a1c4fac5381e0821da43c895bc5c8b42c3d78de
Josue <josuebasilis@gmail.com>
2021-06-28 08:33:02 -0400

    Third commit

---
+++ day-3/Lab10/Lab10 Switches and Fall Through Logic/Lab-Switch/src/SwitchStatement.java
@@ -0,0 +1,28 @@
+
+public class SwitchStatement {
+	
+	public static void main(String[] args) {
+		
+		int x = 5;
+		
+		switch(x) {
+		case 6: {
+			System.out.println("case 6 ran");
+			break;
+		}
+		default:{ 
+			System.out.println("THe default case ran");
+			break;
+		}
+		case 7:{ 
+			System.out.println("case 7 ran");
+			break;
+		}
+		case 10:{ 
+			System.out.println("case 10 ran");
+			break;
+		}
+		}
+	}
+
+}
