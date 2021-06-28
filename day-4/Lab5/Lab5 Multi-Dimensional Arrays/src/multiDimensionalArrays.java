201f52d1cdb94bfa55041b6edffc4930ee4c5ecd
Josue <josuebasilis@gmail.com>
2021-06-28 08:54:44 -0400

    Forth commit

---
+++ day-4/Lab5/Lab5 Multi-Dimensional Arrays/src/multiDimensionalArrays.java
@@ -0,0 +1,16 @@
+
+public class multiDimensionalArrays {
+	
+	public static void main(String[] args) {
+		int [][][] my3dArray = new int [3][3][3];
+		
+		my3dArray[0][0][0] = 1;
+		System.out.println(my3dArray[0][0][0]);
+		
+		int [][] my2dArray = {{1,2,0} , {4,5,6}, {1,4,5}};
+		System.out.println(my2dArray.length);
+		
+		System.out.println(my2dArray[1][2]);
+	}
+
+}
