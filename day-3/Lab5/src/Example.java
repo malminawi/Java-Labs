f9ae29e1c7cd293faba2b6b7ebe83a53240f1927
Josue <josuebasilis@gmail.com>
2021-06-27 20:26:22 -0400

    Lab 5

---
+++ day-3/Lab5/src/Example.java
@@ -0,0 +1,22 @@
+
+public class Example {
+	public static void main(String[] args) {
+		
+		//initial values
+		int i = 200;
+		//cast to a short
+		short s = (short)i;
+		
+		System.out.println(s);
+		
+		//cast to a double
+		
+		double d = (double)i;
+		System.out.println(d);
+		
+		//cast to a byte
+		byte b = (byte)i;
+		System.out.println(b);
+	}
+
+}
