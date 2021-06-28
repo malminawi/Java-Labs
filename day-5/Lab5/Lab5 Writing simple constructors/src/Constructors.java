91bc445a5d4a1a7b13891f1ad449aecd3e328ebd
Josue <josuebasilis@gmail.com>
2021-06-28 09:48:35 -0400

    Constructors

---
+++ day-5/Lab5/Lab5 Writing simple constructors/src/Constructors.java
@@ -0,0 +1,19 @@
+
+public class Constructors {
+
+	public Constructors(){
+        System.out.println("Default constructor ran.");
+    }
+
+    public Constructors(int value){
+        System.out.println(value);
+    }
+
+    public static void main(String[] args) {
+        //create instances here
+        Constructors c = new Constructors(5839);
+
+        //use the no-arg constructor
+        Constructors cNoArg = new Constructors();
+    }
+}
